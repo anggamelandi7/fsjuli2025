449261e81dd4428101f7174cef505c81cf493bf6
anggamelandi <melandy.angga21@gmail.com>
2025-07-05 14:13:16 +0700

    algoritma dan psecode

---
+++ algoritmapseudecode.js
@@ -0,0 +1,63 @@
+// algoritma
+
+/**
+ *  Study case 1
+ * 
+ * Tentukan algoritma dan pseudecode untuk menghitung luas persegi panjang
+ * 
+ * algoritma = 
+ * 
+Mulai
+
+Masukkan panjang
+
+Masukkan lebar
+
+Hitung luas = panjang × lebar
+
+Tampilkan hasil luas
+
+Selesai
+
+
+ * Pseudoceode = 
+Tampilkan "Masukkan panjang:"
+  Baca panjang
+
+  Tampilkan "Masukkan lebar:"
+  Baca lebar
+
+  luas = panjang × lebar
+
+  Tampilkan "Luas persegi panjang adalah:", luas
+Selesai
+ */
+
+
+/**
+ *  Study case 2
+ * 
+ * Tentukan algoritma dan pseudecode untuk menghitung luas lingkaran
+ * algoritma = 
+ * 
+Mulai
+
+Masukkan nilai jari-jari (r)
+
+Hitung luas = π × r × r
+
+Tampilkan hasil luas
+
+Selesai
+
+ * Pseudoceode = 
+Mulai
+  Tampilkan "Masukkan jari-jari lingkaran:"
+  Baca r
+
+  luas ← 3.14 × r × r
+
+  Tampilkan "Luas lingkaran adalah:", luas
+Selesai
+ */
+
