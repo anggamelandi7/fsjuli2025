1f4b97f1885bb0fe6f5357947c79e80ae61c2630
anggamelandi <melandy.angga21@gmail.com>
2025-07-05 15:16:10 +0700

    algoritma dan psecode

--- algoritmapseudecode.js
+++ algoritmapseudecode.js
@@ -60,14 +60,73 @@ Mulai
 Selesai
  */
 
+
+/**
+ *  STUDY CASE 3
+ * 
+ * Tentukan algoritma dan pseudecode untuk menghitung 1/2 volume bola
+ * algoritma = 
+ * 
+Mulai
+
+Masukkan nilai jari-jari (r)
+
+Hitung volume = (1/2) × (4/3) × π × r³
+
+Tampilkan hasil volume
+
+Selesai
+
+
+ * Pseudoceode = 
+Mulai
+  Tampilkan "Masukkan jari-jari bola:"
+  Baca r
+
+  volume ← (1/2) × (4/3) × 3.14 × r × r × r
+
+  Tampilkan "Volume setengah bola adalah:", volume
+Selesai
+ */
+
+/**
+ *  STUDY CASE 4
+ * 
+ * Tentukan algoritma dan pseudecode luas permukaan tabung
+ * algoritma = 
+ * 
+Mulai
+
+Masukkan jari-jari (r)
+
+Masukkan tinggi (t)
+
+Hitung luas = 2 × π × r × (r + t)
+
+Tampilkan hasil luas
+
+Selesai
+
+ * Pseudoceode = 
+ Mulai
+  Tampilkan "Masukkan jari-jari tabung:"
+  Baca r
+
+  Tampilkan "Masukkan tinggi tabung:"
+  Baca t
+
+  luas ← 2 × 3.14 × r × (r + t)
+
+  Tampilkan "Luas permukaan tabung adalah:", luas
+Selesai
+ */
 /**
  *  STUDY CASE 5
  * 
- * Tentukan algoritma dan pseudecode luas permukaan balok
- * 
+ * Tentukan algoritma dan pseudecode luas permukaan balok :
  * algoritma = 
  * 
-MMulai
+Mulai
 
 Masukkan nilai panjang (p)
 
@@ -80,7 +139,6 @@ Hitung luas = 2 × (p × l + p × t + l × t)
 Tampilkan hasil luas
 
 Selesai
-
 
  * Pseudoceode = 
  Mulai
@@ -98,6 +156,5 @@ Selesai
   Tampilkan "Luas permukaan balok adalah:", luas
 Selesai
  */
-
 
 
