0bdc716b5aa00714ee8695afca94294fa9b6bb1c
anggamelandi <melandy.angga21@gmail.com>
2025-07-06 11:03:12 +0700

    variable

--- variable.js
+++ variable.js
@@ -6,10 +6,108 @@
 // console.log(volume);
 // ===================
 
-let r = 5;
-const pi = 3.14;
-let luas = pi * r * r;
-console.log(luas);
+
+
+//luas lingkaran
+// let r = 5;
+// const pi = 3.14;
+// let luas = pi * r * r;
+// console.log(luas);
+
+
+//=============================================================//
+
+/**
+ * STUDY CASE 1 
+ * Tentukan algoritma dan Psedocode dan Coding untuk menghitung Luas Permukaan Balok
+ * 
+ * Algoritma :
+ * 1. masukkan nilai panjang  (p)
+ * 2. masukkan nilai lebar (l)
+ * 3. masukkan nilai tinggi (t)
+ * 4. Hitung Luas = 2 x(p x l + p x t + l x t)
+ * 5. Tampilan hasil luas
+ * 6. Selesai
+ * 
+ * Psedeucode
+ * Mulai
+ * Tampilkan "Masukkan panjang balok:" Baca p
+ * Tampilkan "Masukkan lebar balok:" Baca l
+ * luas ← 2 × (p × l + p × t + l × t)
+ * Tampilkan "Luas permukaan balok adalah:", luas
+ * selesai
+ */
+
+//CODING
+// let p = 10;
+// let l = 5;
+// let t = 3;
+
+// let luaspermukaanbalok = 2 * (p *l + p * t + l);
+// console.log(luaspermukaanbalok);
+
+
+/**
+ * STUDY CASE 2 
+ * Tentukan algoritma dan Psedocode dan Coding untuk menghitung massa jenis
+ * 
+ * Algoritma :
+ * 1. buat variable massa
+ * 2. buat variable volume
+ * 3. buat variable rho
+ * 4. Hitung rho
+ * 5. Rumus Massa Jenis = massa /volume
+ * 6. Tampilkan rho
+ * 
+ * Psedeucode
+ * BEGIN
+ * STORE massa WITH ANY NUMBER
+ * STORE volume WTIH ANY NUMBER
+ * SET rho TO massa DIV volume
+ * PRINT rho
+ * End
+ */
+
+//CODING
+// let massa = 10;
+// let volume = 3;
+
+
+// let rho = massa / volume
+// console.log(rho.toFixed(2)); // to fix untuk membatasi angka dibelakang koma
+
+
+/**
+ * STUDY CASE 3
+ * Tentukan algoritma dan Psedocode dan Coding untuk menghitung volume tabung
+ * 
+ * Algoritma :
+ * 1. buat variable r
+ * 2. buat variable t
+ * 3. buat variable phi
+ * 4. hitung variable tabung
+ * 5. rumus volume tabung = phi * r * r* t
+ * 6. tampilkan volume tabung
+ * 
+ * Psedeucode
+ * BEGIN
+ * STORE phi with 3.14
+ * STORE with any number
+ * STORE t with any number
+ * 
+ * set volume to phi mul r mul r mul 
+ * print volume
+ * End
+ */
+
+//CODING
+// const phi = 3.14;
+// let r = 7;
+// let t = 5;
+
+
+// let volume = phi * r * r* t
+// console.log(volume.toFixed(2));
 
 
 
