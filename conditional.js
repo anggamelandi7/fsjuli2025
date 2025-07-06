44dfa590306b3d98f2e9ee30ecb294bb9cb79012
anggamelandi <melandy.angga21@gmail.com>
2025-07-06 16:02:22 +0700

    conditional

--- conditional.js
+++ conditional.js
@@ -193,3 +193,229 @@ PSEUDOCODE
 // } else if (power >= 30) {
 //   console.log("Character is Beginner");
 // }
+
+
+/* Study Case 4
+ *
+ * Ada 3 jenis access : Public, Protected, Private.
+ * Jika access kosong, maka tampilkan "Input access"
+ *
+ * Access public, jika level di bawah 5 maka tampilkan "Public di bawah 5"
+ * Access public, jika level di atas 5 maka tampilkan "Public di atas 5"
+ * Access public, jika level sama dengan 5 maka tampilkan "Public five"
+ *
+ * Access private, jika level di bawah 5 maka tampilkan "Private di bawah 5"
+ * Access private, jika level di atas 5 maka tampilkan "Private di atas 5"
+ * Access private, jika level sama dengan 5 maka tampilkan "Private five"
+ *
+ * Access protected, jika level di bawah 5 maka tampilkan "Protected di bawah 5"
+ * Access protected, jika level di atas 5 maka tampilkan "Protected di atas 5"
+ * Access protected, jika level sama dengan 5 maka tampilkan "Protected five"
+ *
+ * Jika access salah, tampilkan "Access is not defined"
+ *
+ * BUAT CODING menggunakan Nested Conditional : Switch case dan if else
+ */
+
+// Pseudocode
+/**
+ * STORE access WITH ANY STRING
+ * STORE level WITH ANY NUMBER
+ *
+ * IF access EMPTY STRING THEN
+ *  DISPLAY "Input Access"
+ * ELSE IF access EQUAL "Public" THEN
+ *  IF level LESS THAN 5 THEN
+ *      DISPLAY "Public di bawah 5"
+ *  ELSE IF level MORE THEN 5 THEN
+ *      DISPLAY "Public di atas 5"
+ *  ELSE
+ *      DISPLAY "Public five"
+ *  ENDIF
+ * ELSE IF access EQUAL "Protected" THEN
+ *  IF level LESS THAN 5 THEN
+ *      DISPLAY "Protected di bawah 5"
+ *  ELSE IF level MORE THEN 5 THEN
+ *      DISPLAY "Protected di atas 5"
+ *  ELSE
+ *      DISPLAY "Protected five"
+ *  ENDIF
+ * ELSE IF access EQUAL "Private" THEN
+ *  IF level LESS THAN 5 THEN
+ *      DISPLAY "Private di bawah 5"
+ *  ELSE IF level MORE THEN 5 THEN
+ *      DISPLAY "Private di atas 5"
+ *  ELSE
+ *      DISPLAY "Private five"
+ *  ENDIF
+ * ELSE
+ *  DISPLAY "Access is not defined"
+ * ENDIF
+ */
+
+// Coding
+// var access = "Public";
+// var level = 5;
+
+// switch (access) {
+//     case "Public":
+//         if (level < 5) {
+//             console.log(access + " di bawah 5");
+//         } else if (level > 5) {
+//             console.log(access + " di atas 5");
+//         } else {
+//             console.log(access + " five");
+//         }
+//         break;
+//     case "Private":
+//         if (level < 5) {
+//             console.log(access + " di bawah 5");
+//         } else if (level > 5) {
+//             console.log(access + " di atas 5");
+//         } else {
+//             console.log(access + " five");
+//         }
+//         break;
+//     case "Protected":
+//         if (level < 5) {
+//             console.log(access + " di bawah 5");
+//         } else if (level > 5) {
+//             console.log(access + " di atas 5");
+//         } else {
+//             console.log(access + " five");
+//         }
+//         break;
+//     default:
+//         if(access === ""){
+//             console.log("Input access")
+//         }else {
+//             console.log("Input is not defined")
+//         }
+//         break;
+// }
+
+/**
+ * * STUDY CASE 5
+ * 
+ * *Sebuah lembaga layanan IT Services yang bergerak di bidang perbaikan laptop dan gadget memberikan layanan khusus berdasarkan dari status pembeli.
+ * 
+ * Pembeli merupakan penduduk WNA, dan memiliki ketentuan berikut:
+ * Lama tinggal di indonesia 3 tahun kurang, maka tidak dapat diskon.
+ * Lama tinggal di indonesia 3 tahun hingga 5 tahun, maka dapat diskon. Jika memiliki syarat :
+ *      usia dibawah 21 tahun, makka diskon 10%
+ *      usia dibawah 21 tahun, makka diskon 10%
+ * lama tinggal di indonesia diatas 5 tahun. jika memliki syarat :
+ *      usia di bawah 21 tahun, makan diskon 5%
+ *      usia di atas 21 tahun, maka diskon 2.5%
+ * Pembeli merupakan penduduk WNI, dan memliki ketentuan berikut:
+ *      WNI tinggal di daerah Barat Indonesia, mendapatkan diskon 10%
+ *      WNI tinggal di daerah Tengah Indonesia, mendapatkan diskon 15%
+ *      WNI tinggal di daerah Tengah Indonesia, mendapatkan diskon 17%
+ * 
+ * contoh perhitungan:
+ * Warga merupakan WNA, lama tinggal 3 tahun dan umur 27 tahun, total pembelian : Rp.100.000
+ * maka mendapatkan diskon 5%, sehingga total yang dibayar adalah Rp.95.000
+ * 
+ */
+
+
+/**
+ *  * Study Case 5
+ *
+ * * Sebuah Lembaga layanan IT Services yang bergerak di bidang perbaikan laptop dan gadget memberikan layanan khusus berdasarkan dr status pembeli.
+ *
+ * Pembeli merupakan penduduk WNA, dan memiliki ketentuan berikut:
+ *	Lama tinggal di Indonesia 3 tahun kurang, maka Tidak Dapat Diskon
+ *	Lama tinggal di Indonesia 3 hingga 5 tahun, maka dapat Diskon. Jika memiliki syarat:
+ *		Usia di Bawah 21 tahun, maka diskon 10%
+ *		Usia di Atas 21 tahun, maka diskon 5%
+ *	Lama tinggal di Indonesia di atas 5 tahun. Jika memiliki syarat:
+ *		Usia di Bawah 21 tahun, maka diskon 5%
+ *		Usia di Atas 21 tahun, maka diskon 2.5%
+ * Pembeli merupakan penduduk WNI, dan memiliki ketentuan berikut:
+ * 	WNI tinggal di daerah Barat Indonesia, mendapakan diskon 10%,
+ *	   WNI tinggal di daerah Tengah Indonesia, mendapatkan diskon 15%,
+ * 	WNI tinggal di daerah Timur Indonesia, mendapatkan diskon 17.5%
+ *
+ * Contoh perhitungan:
+ * Warga merupakan WNA, lama tinggal 3 tahun dan umur 27 tahun. Total pembelian : Rp. 100.000
+ * Maka mendapatkan diskon 5%, sehingga total yang di bayar adalah Rp. 95.000
+ *
+ * PSEUDOCODE*
+ *
+ * BEGIN
+ *  STORE status WITH ANY STRING
+ *  STORE period WITH ANY NUMBER
+ *  STORE age WITH ANY NUMBER
+ *  STORE location WITH ANY STRING
+ *  STORE price WITH ANY NUMBER
+ *
+ * IF status EQUAL "WNA" THEN
+ *      IF period LESS THAN 3 THEN
+ *          price EQUAL price
+ *      ELSE IF period GREATER THAN  5 THEN
+ *          IF age LESS THAN 21 THEN
+ *              price EQUAL price MUL 0.95
+ *          ELSE IF age GREATER THAN EQUAL 21 THEN
+ *              price EQUAL price MUL 0.975
+ *          END IF
+ *      ELSE IF period GREATER THAN EQUAL 3 AND period LESS THAN EQUAL 5 THEN
+ *          IF age LESS THAN 21 THEN
+ *              price EQUAL price MUL 0.9
+ *          ELSE IF age GREATER THAN EQUAL 21 THEN
+ *              price EQUAL price MUL 0.95
+ *          END IF
+ *      END IF
+ * ELSE IF status EQUAL "WNI" THEN
+ *      IF location EQUAL "barat" THEN
+ *          price EQUAL price MUL 0.9
+ *      ELSE IF location EQUAL "tengah" THEN
+ *          price EQUAL price MUL 0.85
+ *      ELSE IF location EQUAL "timur" THEN
+ *          price EQUAL price MUL 0.825
+ *      END IF
+ * END IF
+ *
+ * END
+ */
+
+// CODING
+
+// let status = "WNI";
+// let period = 3;
+// let discount = 0;
+// let age = 25;
+// let location = "barat";
+// let price = 100000;
+
+// switch (status) {
+//   case "WNA":
+//     if (period < 3) {
+//       price = price;
+//     } else if (period >= 3 && period <= 5) {
+//       if (age < 21) {
+//         price = price * 0.9;
+//       } else if (age >= 21) {
+//         price = price * 0.95;
+//       }
+//     } else if (period < 5) {
+//       if (age < 21) {
+//         price = price * 0.95;
+//       } else if (age >= 21) {
+//         price = price * 0.97;
+//       }
+//     }
+//     break;
+//   case "WNI":
+//     if (location === "barat") {
+//       price = price * 0.9;
+//     } else if (location === "tengah") {
+//       price = price * 0.85;
+//     } else if (location === "timur") {
+//       price = price * 0.825;
+//     }
+//     break;
+// }
+// console.log(price);
+
+
