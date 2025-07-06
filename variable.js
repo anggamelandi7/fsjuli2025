77ebb045f27d4fa4209ba59b997a8545830ef83d
anggamelandi <melandy.angga21@gmail.com>
2025-07-06 10:09:27 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+
+// volume kubu
+
+// var sisi = 5;
+// var volume = sisi * sisi * sisi;
+// console.log(volume);
+// ===================
+
+let r = 5;
+const pi = 3.14;
+let luas = pi * r * r;
+console.log(luas);
+
+
+
